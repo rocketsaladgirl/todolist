1af55bd354b723715a44cccff613e8ddaf7ff400
rocketsaladgirl <114432448+rocketsaladgirl@users.noreply.github.com>
2023-10-03 16:56:44 +0300

    добавлен функционал переноса данных между блоками To Do и Done методом doCheck

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ Vue.createApp({
     data() {
         return {
             valueInput: '',
-            needDoList: []
+            needDoList: [],
+            completeList: []
 
         };
     },
@@ -17,6 +18,16 @@ Vue.createApp({
                 id: Math.random()
             });
             this.valueInput = '';
+        },
+        doCheck (index, type) {
+            if(type === 'need') {
+                const completeMask = this.needDoList.splice(index, 1);
+                this.completeList.push(...completeMask);
+            }
+            else {
+                const noCompleteMask = this.completeList.splice(index, 1);
+                this.needDoList.push(...noCompleteMask);
+            }
         }
 
     }
