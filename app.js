d7f77722f3611ebb37d2f79548cadc7e8a007fa4
rocketsaladgirl <114432448+rocketsaladgirl@users.noreply.github.com>
2023-10-03 17:15:22 +0300

    Добпалена работа кнопки btn-remove и функционал для счетчика mask-num

--- app.js
+++ app.js
@@ -28,6 +28,10 @@ Vue.createApp({
                 const noCompleteMask = this.completeList.splice(index, 1);
                 this.needDoList.push(...noCompleteMask);
             }
+        },
+        removeMask (index, type) {
+            const toDoList = type === 'need' ? this.needDoList : this.completeList;
+            toDoList.splice(index, 1);
         }
 
     }
